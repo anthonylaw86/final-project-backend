08f9c5ebc1afcf7883a330069b1241e26e6c6ce3
AnthonyLaw <anthonylaw910@gmail.com>
2024-07-22 10:20:16 -0600

    auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,29 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+
+const { UnauthorizedError } = require("../utils/Errors/unauthorizedError");
+
+module.exports.authorization = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("Authorization required"));
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return next(new UnauthorizedError("Authorization required"));
+  }
+
+  req.user = payload;
+
+  return next();
+};
+
 var generateRandomString = function (length) {
   var text = "";
   var possible =
