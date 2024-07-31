13c7912c4f723b4a3c070cabd0bef8e21de3e63f
AnthonyLaw <anthonylaw910@gmail.com>
2024-07-31 11:28:38 -0600

    index adjustments

--- server/index.js
+++ server/index.js
@@ -1,14 +1,57 @@
 require("dotenv").config();
 const express = require("express");
 const dotenv = require("dotenv");
-const request = require("request");
+// const request = require("request");
+const fetch = import("node-fetch");
 const { URLSearchParams } = require("url");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
 const helmet = require("helmet");
+const indexRouter = require("../routes/index");
+const errorHandler = require("../middleware/errorHandler");
+const limiter = require("../middleware/rateLimiter");
 
-const port = 3002;
+const { requestLogger, errorLogger } = require("../middleware/logger");
+
+const app = express();
+const { port = 3002 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/beatapp")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+// RATE LIMITER
+app.use(limiter);
+
+// SET SECURITY HEADERS
+app.use(helmet());
+
+// BASIC REQUEST LOGGING
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} - ${JSON.stringify(req.body)}`);
+  next();
+});
+
+// REQUEST LOGGER
+app.use(requestLogger);
+
+// ROUTES
+app.use("/", indexRouter);
+
+// ERROR LOGGER
+app.use(errorLogger);
+
+// CELEBRATE ERROR HANDLER
+app.use(errors());
+
+// CENTRALIZED ERROR HANDLER
+app.use(errorHandler);
+
+// SPOTIFY
 
 global.access_token = "";
 
@@ -30,8 +73,8 @@ var generateRandomString = function (length) {
   return text;
 };
 
-var app = express();
 app.use(cors());
+app.use(express.json());
 app.options("*", cors());
 
 app.get("/auth/login", (req, res) => {
@@ -73,22 +116,58 @@ app.get("/auth/callback", (req, res) => {
     json: true,
   };
 
-  request.post(authOptions, function (error, response, body) {
-    // if (!error && response.statusCode === 200) {
+  // request.post(authOptions, function (error, response, body) {
+  //   // if (!error && response.statusCode === 200) {
 
-    //   res.redirect("/");
-    // }
-    access_token = body.access_token;
-    if (error) {
+  //   //   res.redirect("/");
+  //   // }
+  //   access_token = body.access_token;
+  //   if (error) {
+  //     console.error("Error:", error);
+  //     return res.status(500).json({ error: "Internal Server Error" });
+  //   }
+  //   if (response.statusCode !== 200) {
+  //     console.error("Failed to retrieve access token:", body);
+  //     return res.status(response.statusCode).json(body);
+  //   }
+
+  //   res.redirect("http://localhost:3000/post-login?token=${token}");
+  // });
+
+  app.post("/profile", async (req, res) => {
+    const authOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          new Buffer(client_id + ":" + client_secret).toString("base64"),
+      },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code: req.body.code,
+        redirect_uri: redirect_uri,
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        "https://accounts.spotify.com/api/token",
+        authOptions
+      );
+      const body = await response.json();
+
+      if (!response.ok) {
+        console.error("Failed to retrieve access token:", body);
+        return res.status(response.status).json(body);
+      }
+
+      const access_token = body.access_token;
+      res.redirect(`http://localhost:3000/post-login?token=${access_token}`);
+    } catch (error) {
       console.error("Error:", error);
-      return res.status(500).json({ error: "Internal Server Error" });
+      res.status(500).json({ error: "Internal Server Error" });
     }
-    if (response.statusCode !== 200) {
-      console.error("Failed to retrieve access token:", body);
-      return res.status(response.statusCode).json(body);
-    }
-
-    res.redirect("http://localhost:3000/post-login?token=${token}");
   });
 });
 
